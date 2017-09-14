e583bfeecd0a1ded45a967ec70561cd59a0b3355
Abhinav Srivastava <abhinav.srivastava309@gmail.com>
2017-09-14 05:57:57 +0530

    Add basic login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
-
   root 'static_pages#home', as: 'home'
   get  '/signup', to: 'users#new', as: 'signup'
   resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
