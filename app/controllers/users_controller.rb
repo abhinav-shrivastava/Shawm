441c05b34eadc02d9a8f3875ff59d0e67632ef99
Abhinav Srivastava <abhinav.srivastava309@gmail.com>
2017-09-18 01:48:01 +0530

    Fixes bug of redirection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = logged_in? ? current_user : User.find(params[:id])
     @searches = @user.searches.paginate(page: params[:page])
   end
 
